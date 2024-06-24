056a0e8325263d5ca623f8c3ddeed35ec96eccb3
Samir Magdy <>
2024-06-24 17:38:21 +0300

    update code using fastapi

--- app.py
+++ app.py
@@ -1,39 +1,29 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
 from transformers import ViTImageProcessor, ViTForImageClassification
 from PIL import Image
 import torch
 import io
 
-app = Flask(__name__)
+app = FastAPI()
 
-# Load the model and processor
 processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
 model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
 
-@app.route('/upload', methods=['POST'])
-def upload_image():
-    if 'image' not in request.files:
-        return jsonify({"error": "No image file found"}), 400
+@app.post("/classify-image/")
+async def classify_image(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes))
 
-    file = request.files['image']
-    if file.filename == '':
-        return jsonify({"error": "No selected file"}), 400
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
 
-    try:
-        image = Image.open(io.BytesIO(file.read()))
+    predicted_class_idx = logits.argmax(-1).item()
+    predicted_class_label = model.config.id2label[predicted_class_idx]
 
-        # Preprocess the image and make prediction
-        inputs = processor(images=image, return_tensors="pt")
-        outputs = model(**inputs)
-        logits = outputs.logits
+    return JSONResponse(content={"predicted_class": predicted_class_label})
 
-        predicted_class_idx = logits.argmax(-1).item()
-        predicted_class_label = model.config.id2label[predicted_class_idx]
-
-        return jsonify({"predicted_class": predicted_class_label}), 200
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app)
